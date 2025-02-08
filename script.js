c99b352aa8fb948e29a6f61955eac879bddcaadf
offsideTV <hexih81020@gmail.com>
2025-02-08 02:12:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// JavaScript para hacer funcionar el menú desplegable
+// JavaScript para hacer funcionar el menú desplegable de eventos
 var acc = document.getElementsByClassName("accordion");
 for (var i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
@@ -8,6 +8,19 @@ for (var i = 0; i < acc.length; i++) {
             panel.style.display = "none";
         } else {
             panel.style.display = "block";
+        }
+    });
+}
+
+// JavaScript para hacer funcionar el menú desplegable de subeventos (eventos específicos)
+var eventBtns = document.getElementsByClassName("event-btn");
+for (var i = 0; i < eventBtns.length; i++) {
+    eventBtns[i].addEventListener("click", function() {
+        var subEvents = this.nextElementSibling; // Encuentra el contenedor de los subeventos
+        if (subEvents.style.display === "block") {
+            subEvents.style.display = "none"; // Oculta los subeventos si están visibles
+        } else {
+            subEvents.style.display = "block"; // Muestra los subeventos si están ocultos
         }
     });
 }
