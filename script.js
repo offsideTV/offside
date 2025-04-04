2d9e91ca8e33425ee493df19595208f4e8756859
offsideTV <hexih81020@gmail.com>
2025-04-04 01:18:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -253,15 +253,3 @@ if (isMobile()) {
 
 
 
-function actualizarReloj() {
-    const ahora = new Date();
-    const horas = ahora.getHours().toString().padStart(2, "0");
-    const minutos = ahora.getMinutes().toString().padStart(2, "0");
-    const segundos = ahora.getSeconds().toString().padStart(2, "0");
-    document.getElementById("reloj").textContent = `${horas}:${minutos}:${segundos}`;
-}
-
-setInterval(actualizarReloj, 1000); // Actualizar cada segundo
-actualizarReloj(); // Llamada inicial para evitar retraso
- 
-}
